90868e99cac532dd2b6e1b96f5ff24ec5431443a
JP <j.peregrino95@hotmail.com>
2017-07-14 17:34:27 +0100

    Added More Analytics tools

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -33,13 +33,13 @@
     <script>
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:564447,hjsv:5};
+            h._hjSettings={hjid:564449,hjsv:5};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
         })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-    </script><!-- Hotjar Tracking Code for www.shopswift.co.uk/ -->
+    </script><!-- Hotjar-->
 
 
 </head>
