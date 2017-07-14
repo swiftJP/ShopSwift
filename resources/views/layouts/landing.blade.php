457ab547938a45a084851c62ab455c21065b040a
JP <j.peregrino95@hotmail.com>
2017-07-14 17:27:55 +0100

    Added Analytics tools

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -20,6 +20,28 @@
     <link rel="shortcut icon" href="{{ asset('icons/SSFavicon.jpg') }}">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <!---Analytics--->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-101448279-1', 'auto');
+        ga('send', 'pageview');
+    </script><!--Google-->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:564447,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    </script><!-- Hotjar Tracking Code for www.shopswift.co.uk/ -->
+
+
 </head>
 
 <body>
